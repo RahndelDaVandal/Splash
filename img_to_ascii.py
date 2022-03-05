fa0f53210a91155529cd1eb4bc44b131091d47f6
Connor Sahleen <Rcsfanz@Gmail.com>
2022-03-04 22:40:03 -0600

    Fixed spelling in img_to_ascii.py file name

--- img_to_ascii.py
+++ img_to_ascii.py
@@ -6,8 +6,8 @@ from typing import Optional
 def convert(
             img_path:Path,
             outfile:Path,
-            new_width:int = field(default = 80),
-            user_chars:Optional[list],
+        user_chars:Optional[list] = None,
+            new_width:int = 80,
              ) -> None:
     """Convert Image to ANSII Art.
 
@@ -15,14 +15,14 @@ def convert(
         img_path(pathlib.Path): path to img file
         outfile(pathlib.Path): path to outfile
             i.e. ~/Documents/outfile.txt
-        new_width(int): width of ansii art 
         user_chars(list): list of chars to use
+        new_width(int): width of ansii art 
     """
     img = Image.open(img_path).convert('L')
     width, height = img.size
-    aspect_ration = height / width
+    aspect_ratio = height / width
     new_height = int(aspect_ratio * new_width * 0.55)
-    img = img.resize(new_width, new_height)
+    img = img.resize((new_width, new_height))
     pixels = img.getdata()
 
     if user_chars:
@@ -30,15 +30,18 @@ def convert(
     else:
         chars = ["*", "S", "#", "&", "@", "$", "%", "*", "!", ":", "."]
 
+    print(chars) # ADDED FOR TESTING PURPOSES
+
     new_pixels = [chars[pixel // 25] for pixel in pixels]
     new_pixels = ''.join(new_pixels)
 
-    ansii_img = [
-                 new_pixels[index : index + width]
-                 for index in range(0, len(new_pixels), new_width)
-                 ]
+    new_pixels_count = len(new_pixels)
+    ascii_img = [
+        new_pixels[index : index + new_width]
+        for index in range(0, new_pixels_count, new_width)
+    ]
 
-    ansii_img = ''.join(ansii_img)
+    print([line for line in ansii_img])
 
     with outfile.open(mode='w') as file:
         file.write(ansii_img)
